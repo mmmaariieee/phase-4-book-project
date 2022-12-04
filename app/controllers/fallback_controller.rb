bd45ab9c8362812961ce195705d4b6cca564faf4
Mariia <m.goncharenkoo@icloud.com>
2022-12-03 21:57:32 -0500

    Configured client-side routing

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -3,7 +3,3 @@ class FallbackController < ActionController::Base
       render file: 'client/public/index.html'
     end
 end
-
-# /Users/macos/Development/code/phase-4/phase-4-project/phase-4-book-project/client/public/index.html
-# ../../../client/public/index.html
-# ././././client/public/index.html
